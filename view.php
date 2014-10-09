9cd9266db4ac5b5cb1eb7755a0e048ddb7322d58
Виталий <millionaire@mail.ru>
2014-10-09 16:52:51 +0700

    Create view.php

---
+++ view.php
@@ -0,0 +1,23 @@
+<?php
+	Class ViewDirect
+	{
+		private $variables = array();
+		private $file_include;
+		
+		function __construct($file_include)
+		{
+			$this->file_include = $file_include;
+		}
+		
+		function assign($name,$value)
+		{
+			$this->variables[$name] = $value;	
+			
+		}		
+		
+		function show_display()
+		{				
+			require_once 'Template.php';
+		}
+	}
+?>
